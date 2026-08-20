ffef48bb7815a9c0763e274c78d3e35277c89e80
kimplayzros-lgtm <kimplayzros@gmail.com>
2026-08-20 16:44:46 +0800

    Enforce home-page name gate before profile access

--- app/middlewares/ProfileAccessMiddleware.php
+++ app/middlewares/ProfileAccessMiddleware.php
@@ -10,8 +10,24 @@ class ProfileAccessMiddleware
             session_start();
         }
 
-        if (isset($_GET['access']) && $_GET['access'] === '1') {
-            $_SESSION['profile_access'] = true;
+        $submittedName = trim((string) ($_GET['student_name'] ?? $_POST['student_name'] ?? ''));
+        $expectedName = 'CYRUS KIM ADRIAN D. SULIT';
+
+        if (!empty($submittedName)) {
+            if (strcasecmp($submittedName, $expectedName) === 0) {
+                $_SESSION['profile_access'] = true;
+                $_SESSION['student_name'] = $expectedName;
+            } else {
+                unset($_SESSION['profile_access']);
+                unset($_SESSION['student_name']);
+                header('Location: /');
+                exit;
+            }
+        } else {
+            unset($_SESSION['profile_access']);
+            unset($_SESSION['student_name']);
+            header('Location: /');
+            exit;
         }
 
         if (empty($_SESSION['profile_access'])) {
